a8d672bf5c92ab86d39a088fe6b6336a82b9cd71
onoroff <adam.grunke@gmail.com>
2019-07-22 15:17:26 -0700

    PUT and DELETE routes created and tested.

--- routes/trips.js
+++ routes/trips.js
@@ -23,14 +23,6 @@ router.post('/', (req, res) => {
         time: req.body.time,
         latDest: req.body.latDest,
         longDest: req.body.longDest
-        // zipDest: '99999',
-        // zipStart: '88888',
-        // latStart: '45.222',
-        // longStart: '-122.333',
-        // time: '23',
-        // latDest: '55.333',
-        // longDest: '-123.222'
-
     },
     function(err, trips) {
         if (err) res.json(err)
@@ -38,19 +30,31 @@ router.post('/', (req, res) => {
     })
 });
 
-// app.post('/cameras', (req, res) => {
-//     Cameras.create({
-//     brand: req.body.brand,
-//     model: req.body.model,
-//     quality: req.body.quality,
-//     format: req.body.format,
-//     megaPixels: req.body.megaPixels,
-//     lenses: req.body.lenses
-//     // lenses: [{brand: "Lumix", focalLength: 15, type: "standard"}]
-//     }, function(err, cameras) {
-//         if (err) res.json(err)
-//         res.json(cameras)
-//     })
-// })
+router.put('/:id', (req, res) => {
+    Trips.findByIdAndUpdate(req.params.id, 
+        {
+            zipDest: req.body.zipDest,
+            zipStart: req.body.zipStart,
+            latStart: req.body.latStart,
+            longStart: req.body.longStart,
+            time: req.body.time,
+            latDest: req.body.latDest,
+            longDest: req.body.longDest
+        }, 
+        function(err, cameras) {
+        if (err) res.json(err)
+        res.json({type: 'success', message: 'You accessed the protected PUT edit TRIPS route'})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Trips.findOneAndRemove({
+        _id: req.params.id
+    },
+    function(err) {
+        if (err) res.json(err);
+        res.json({type: 'success', message: 'You deleted one trip'})
+    })
+})
 
 module.exports = router; 
