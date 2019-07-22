285b9a8973b791b04a542b82afb8acee1bb3f6be
Nathan Chan <nathanchan@Nathans-MacBook-Air.local>
2019-07-22 16:51:41 -0700

    added Raincheck component and updated New Trip component comment

--- client/src/components/NewTrip.jsx
+++ client/src/components/NewTrip.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const NewTrip = props => {
     return (
         <>
-        {/* // Let's return the form that allows people to save a trip */}
+        {/* // Let's return the form that allows people to save a trip. Below is sample code*/}
         <div className="Login">
                 <h3>Create a New Trip</h3>
-                {/* <form onSubmit={this.handleSubmit}> */}
+                <form onSubmit={this.handleSubmit}>
                     <input onChange={this.handleEmailChange}
                             value={props.trips.zip}
                             type="number"
