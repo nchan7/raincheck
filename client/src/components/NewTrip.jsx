9998d84c3e80fe7578e601fb76162294908d030b
Nathan Chan <nathanchan@Nathans-MacBook-Air.local>
2019-07-22 14:55:13 -0700

    created separate components

---
+++ client/src/components/NewTrip.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewTrip = props => {
+    return (
+        <>
+        // Let's return the form that allows people to save a trip
+        </>
+    )
+
+}
+
+export default NewTrip;
---
+++ client/src/components/TripContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TripContainer = props => {
+
+}
+
+export default TripContainer; 
