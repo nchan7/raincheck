1f400295ce1d1206814e333e38eaa0cf149a93ee
onoroff <adam.grunke@gmail.com>
2019-07-23 11:47:23 -0700

    group push/pull. Mapbox zip code to lat/long in process

--- routes/trips.js
+++ routes/trips.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const Trips = require('../models/trip')
 const User = require('../models/user')
+const mapbox = require("@mapbox/mapbox-sdk/services/geocoding");
+const geocodingClient = mapbox({accessToken: process.env.MAPBOX_PUBLIC_KEY});
 
 router.use(express.urlencoded({extended: false}));
 
@@ -24,7 +26,8 @@ router.get('/:id', (req, res) => {
     })
 });
 
-// POST trip for a user
+
+// POST trip for a user - WORKING
 router.post('/', (req, res) => {
     User.findById(req.user, function(err, user){
         Trips.create({
@@ -49,6 +52,51 @@ router.post('/', (req, res) => {
         })
     })
 });
+
+// POST trip for a user - TESTING MAPBOX CONVERSION of zip to lat/long
+// router.post('/', (req, res) => {
+
+//     let locStart = req.body.zipStart; 
+//     // Seattle, WA
+//     // use geocoder to query the location with sushi appended to the query
+//     // then take response from mapbox and render "show" with the data
+//     geocodingClient.forwardGeocode({
+//     query: locStart
+//     }).send().then( function(response) {   
+//         var latStartFromZip = response.body.features[0].center[1];
+//         var longStartFromZip = response.body.features[0].center[0];
+
+//         let locStart = req.body.zipStart; 
+//         geocodingClient.forwardGeocode({
+//         query: locStart
+//         }).send().then( function(response) {   
+//             var latStartFromZip = response.body.features[0].center[1];
+//             var longStartFromZip = response.body.features[0].center[0];
+    
+
+//     User.findById(req.user, function(err, user){
+//         Trips.create({
+//             tripName: req.body.tripName,
+//             zipStart: req.body.zipStart,
+//             latStart: latStartFromZip,
+//             longStart: longStartFromZip,
+//             startTime: req.body.startTime,
+//             travelTime: req.body.travelTime,
+//             zipDest: req.body.zipDest,
+//             latDest: req.body.latDest,
+//             longDest: req.body.longDest,
+//             returnTime: req.body.returnTime,
+//             returnTravelTime: req.body.returnTravelTime
+//         },
+//         function(err, trip) {
+//             user.trips.push(trip)
+//             user.save(function(err, user) {
+//                 if (err) res.json(err)
+//                 res.json({user})
+//             })
+//         })
+//     })
+// });
 
 // UPDATE trip for a user
 router.put('/:id', (req, res) => {
